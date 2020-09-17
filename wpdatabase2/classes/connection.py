30ebf2a95518421f23b8bf5f838d49e0e93cdc21
Shawn Anderson <sanderson@eye-catcher.com>
2020-09-16 20:51:34 -0400

    Updated to WpConnection to allow empty values

--- wpdatabase2/classes/connection.py
+++ wpdatabase2/classes/connection.py
@@ -24,18 +24,6 @@ class WpConnection():
                  credentials):
         self._log = getLogger(__name__)
 
-        if (not db_host):
-            raise InvalidArgumentsError(
-                'Must specify a database host/ip.')
-
-        if (not db_name):
-            raise InvalidArgumentsError(
-                'Must specify a database name.')
-
-        if (not credentials or not isinstance(credentials, WpCredentials)):
-            raise InvalidArgumentsError(
-                'Must specify a database credentials.')
-
         self.db_name = db_name
         self.db_host = db_host
         self.credentials = credentials
@@ -70,7 +58,7 @@ class WpConnection():
     @db_port.setter
     def db_port(self, value):
         """ Gets the database port. """
-        self._db_port = value    
+        self._db_port = value
 
     ###########################################################################
     @property
@@ -82,7 +70,7 @@ class WpConnection():
     @db_name.setter
     def db_name(self, value):
         """ Sets the database name. """
-        self._db_name = value    
+        self._db_name = value
 
     ###########################################################################
     @property
@@ -94,4 +82,4 @@ class WpConnection():
     @credentials.setter
     def credentials(self, value):
         """ Sets the database credentials. """
-        self._credentials = value    
+        self._credentials = value
